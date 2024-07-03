9d22491f8d5b46c8654bb6cb9287bb4b01ab022c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-03 04:20:56 +0300

    Create 0001-simple-counter-App.js

---
+++ 0001-simple-counter-App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import CounterApp from './Counter';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Simple Counter</h1>
+
+      <CounterApp />
+
+    </div>
+  );
+}
+
+export default App;
