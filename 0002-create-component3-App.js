9ab3bab04d24d4f6ea8e315bf35119554b615f3e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-03 05:15:49 +0300

    Create 0002-create-component3-App.js

---
+++ 0002-create-component3-App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import FirstComponent from "./components/First"
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent />
+    </div>
+  )
+}
+
+export default App
