136d87bedb37c4a0e2cea3ab71829faf0f0e6506
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-13 17:13:57 +0300

    Create 0006-props-Areas.js

---
+++ 0006-props-Areas.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Areas({ areaName, areaDesciption }) {
+  return (
+    <>
+      <div>{areaName}</div>
+
+      <div>{areaDesciption}</div>
+    </>
+  );
+}
+
+export default Areas;
