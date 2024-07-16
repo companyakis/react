e49cd65959ca170bbd77fbb0783b8f1124a596f4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-16 14:43:09 +0300

    Create 0012-conditional-rendering2-SimpleSigninPage.js

---
+++ 0012-conditional-rendering2-SimpleSigninPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SimpleSigninPage() {
+  return (
+    <div>
+        <div>
+            User e-mail
+            <input type='email' />
+        </div>
+        <div>
+            User password
+            <input type='password' />
+        </div>
+    </div>
+  )
+}
+
+export default SimpleSigninPage
