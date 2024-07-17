585733964d6eb89fc054643b6bc097022e49fae4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-17 21:12:51 +0300

    Create 0013-props-App.js

---
+++ 0013-props-App.js
@@ -0,0 +1,13 @@
+import Cities from "./Cities";
+
+function App() {
+  return (
+    <div>
+      <Cities
+        userArray={["İzmir", "Ankara", "İstanbul", "Kahramanmaraş", "Adana", "Hatay"]}
+      />
+    </div>
+  );
+}
+
+export default App;
