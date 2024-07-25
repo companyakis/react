422dbae389608ce7282bb7da2f592b7c5fa8d4f5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-25 14:31:19 +0300

    Create 0019-useeffect3.js

---
+++ 0019-useeffect3.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+
+function MultipleAmounts() {
+
+    const [counter, setCounter] = useState(0)
+
+    const [plus, setPlus] = useState(1)
+
+    useEffect(() => {
+      console.log("counter state changed!")
+    }, [counter])
+
+    useEffect(() => {
+      console.log("plus state changed!")
+    }, [plus])
+
+
+  return (
+    <div>
+        <h1>Counter value: {counter}</h1>
+
+        <br/>
+
+        <button onClick={() => setCounter(counter + plus)}>Counter Up</button>
+
+        <br />
+
+        <h2>Plus?</h2>
+
+        <button onClick={() => setPlus(1)}>Plus 1</button>
+
+        <button onClick={() => setPlus(5)}>Plus 5</button>
+
+        <button onClick={() => setPlus(10)}>Plus 10</button> 
+      
+    </div>
+  )
+}
+
+export default MultipleAmounts
