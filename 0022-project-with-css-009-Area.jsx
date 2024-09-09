87e7fef7de2f19a383d3474175411a450e18ca99
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-09-09 10:30:41 +0300

    Update 0022-project-with-css-009-Area.jsx

--- 0022-project-with-css-009-Area.jsx
+++ 0022-project-with-css-009-Area.jsx
@@ -5,7 +5,7 @@ function Area({area}) {
   const {id, title, desc, pic} = area
     
   return (
-    <div>
+    <div className='area-info'>
       <img src={pic} width={200} height={100} alt="lorem ipsum" />
       <h3 className='area-title'>{title}</h3>
       <p className='area-desc'>{desc}</p>
