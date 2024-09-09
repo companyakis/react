186c42e7302e8ca87569466d70bbc85349aa6e7f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-09-09 08:05:32 +0300

    Create 0022-project-with-css-003-Header.jsx

---
+++ 0022-project-with-css-003-Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '/src/App.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='title'>IMPORTANT AREAS</div>
+    </div>
+  )
+}
+
+export default Header
