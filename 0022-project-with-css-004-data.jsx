366d4578efbc707b20ce81a505cd995228982693
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-09-09 08:24:52 +0300

    Create 0022-project-with-css-004-data.jsx

---
+++ 0022-project-with-css-004-data.jsx
@@ -0,0 +1,35 @@
+import AI from './images/ai.jpg'
+import Blockchain from './images/blockchain.jpg'
+import DeFi from './images/defi.jpg'
+import DS from './images/ds.jpg'
+
+export const areas = [
+
+    {
+        id: 100,
+        title: "AI",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, adipisci.",
+        pic: AI
+    },
+
+    {
+        id: 108,
+        title: "Blockchain",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, adipisci.",
+        pic: Blockchain
+    },
+
+    {
+        id: 100,
+        title: "DeFi",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, adipisci.",
+        pic: DeFi
+    },
+
+    {
+        id: 100,
+        title: "Data Science",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, adipisci.",
+        pic: DS
+    }
+]
