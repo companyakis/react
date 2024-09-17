4d9852f543b7827cc9ea287f2af0c2e2dd516b63
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-09-17 23:54:31 +0300

    Create 0805-conditionals.ts

---
+++ 0805-conditionals.ts
@@ -0,0 +1,15 @@
+let a: number = 654.21
+
+let b: number = 214.21
+
+if (a > b) {
+    console.log("a > b")
+}
+
+else if (b > a) {
+    console.log("b > a")
+}
+
+else {
+    console.log("a == b")
+}
