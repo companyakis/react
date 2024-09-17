fcd30faf50e7e9e8f673b3dd6ddf15d4c4e8dc29
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-09-17 20:59:37 +0300

    Create 0804-enum2.ts

---
+++ 0804-enum2.ts
@@ -0,0 +1,12 @@
+const enum AboutHer {
+    Name = "Aybüke",
+    Department = "Sales",
+    ID = 106
+}
+
+console.log(AboutHer.Department) // Sales
+
+let herID: AboutHer = AboutHer.ID
+
+console.log("Her ID is ", herID, ".") // Her ID is  106 .
+
