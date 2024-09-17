ab78a615daa8ce6b66316c0e69a373d3673cb92b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-09-17 20:47:39 +0300

    Create 0803-tuple.ts

---
+++ 0803-tuple.ts
@@ -0,0 +1,11 @@
+let ceo: [string, string, number, boolean] = ["Ayhan Arslan", "Turkiye", 1966, true]
+
+let sales = ["January", 76500]
+
+console.log(ceo)
+
+console.log(sales[0])
+
+// [ 'Ayhan Arslan', 'Turkiye', 1966, true ]
+
+// January
