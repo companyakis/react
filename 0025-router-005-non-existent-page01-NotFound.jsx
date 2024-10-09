24cd3e7b98077cddda1a9079b8095c56529968bf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-09 09:00:24 +0300

    Create 0025-router-005-non-existent-page01-NotFound.jsx

---
+++ 0025-router-005-non-existent-page01-NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+      <p>The page you're looking for doesn't exist. Please, try again.</p>
+    </div>
+  )
+}
+
+export default NotFound
