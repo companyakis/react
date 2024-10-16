dece5b09a18f1eadcaf20a0bf7842af9be71ad55
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-16 23:22:15 +0300

    Create 0024-project-formik-yup-register2-App.jsx

---
+++ 0024-project-formik-yup-register2-App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import UserRegisterForm from './components/UserRegisterForm'
+
+
+function App() {
+
+  return (
+    <>
+      <UserRegisterForm />
+    </>
+  )
+}
+
+export default App
