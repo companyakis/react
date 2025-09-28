8f14bcd99a59cf42b31fd5f0fecdf1a654b14c08
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-28 23:26:36 +0300

    Delete 0802-array2.ts

--- 0802-array2.ts
+++
@@ -1,7 +0,0 @@
-let numberOfEmployees: number[][] = [[2021, 2022, 2023], [8796, 7454, 9787]]
-
-console.log("Number of 2022 employee: ", numberOfEmployees[1][1]) // Number of 2022 employee:  7454
-
-let namesAndAges: (string | number)[][] = [["Aygün", "Kağan", "Hakan", "Bilge"], [34, 28.5, 41, 65]]
-
-console.log(`Kağan is ${namesAndAges[1][1]} years old.`) // Kağan is 28.5 years old.
